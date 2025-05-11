799cdc024718eb88ac5dff5023cc764ff75eebc9
SURYASSACHINP22 <suryawanshsachinp07@gmail.com>
2025-05-11 15:31:09 +0530

    Checked and made changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
-  base: './',
+  base: '/',
   publicDir: 'public',
   plugins: [
     react(),
